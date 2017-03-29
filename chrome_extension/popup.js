ae239f95c219b574477bff225828e6ffa2e5b5d9
Antonio Cuccinielello <antonio.cucciniello16@gmail.com>
2017-03-29 11:18:32 -0400

    Using 'xhr' to send GET for news data to API     TODO: send news data to client

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,16 +1,8 @@
 function getHello () {
-  var xhr = new window.XMLHttpRequest()
-  xhr.open('GET', 'http://localhost:8000/extension', true)
-  xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8')
-  xhr.onreadystatechange = function () {
-    if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-      console.log(xhr.responseText)
-    }
-  }
-  xhr.onload = function() {
-    console.log('it was sent')
-  }
-  xhr.send()
+  var xhr  = require('xhr')
+  xhr.get('http://localhost:8000/extension', function(err, resp) {
+    console.log(resp.body)
+  })
 }
 
 document.addEventListener('DOMContentLoaded', function () {
--- web_app/extension/views.py
+++ web_app/extension/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+import os
 # Create your views here.
 
 def index(request):
+  apiKey = os.environ['API_KEY']
+  url = 'https://newsapi.org/v1/articles?source=business-insider&apiKey=' + apiKey
+  r = requests.get(url)
+  print "Here is the return: %s" % r.json()
   return HttpResponse("Hello world you are at the polls index.")
